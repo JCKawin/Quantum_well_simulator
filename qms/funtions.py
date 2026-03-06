df8752ba586f1f9a89c10bc38b7d3a09be0e92c6
JCKawin <jckawin2007@gmail.com>
2026-03-06 13:58:21 +0530

    basic structrue

---
+++ qms/funtions.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.linalg as la
+import qutip
+import mpmath
+
+def potential(x):
+    pass
+
+def second_derivative(psi):
+    pass
+
+def build_hamiltonian():
+    pass
+
+def normalize_wavefunction():
+    pass
+
+def probability_density():
+    pass
+
+def expectation_x():
+    pass
+
+def expectation_energy():
+    pass
+
+def finite_difference_solver():
+    pass
